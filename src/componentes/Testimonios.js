7b437ea6f01cca791178ab81358cf712f8e7c35a
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-06-04 14:19:12 -0300

    Agrergo datos en js

--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -6,7 +6,7 @@ function Testimonio(props) {
     <div className='contenedor-testimonio'>
       <img className="imagen-testimonio"
       src={require(`../imagenes/${props.imagen}.jpg`)}
-      alt='Porta Inmundo' />
+      alt='Foto Testimonio' />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
           <strong>{props.nombre}</strong> en {props.lugar}</p>
---
+++ src/componentes/datosTestimonios.js
@@ -0,0 +1,48 @@
+const testimonio = {
+    "datos": [
+        {
+            
+                "id": 1,
+                "nombre": "Porta",
+                "lugar": "Dique La Isla",
+                "imagen": "../imagenes/Porta",
+                "profesion": "Come traba de 2 pesos",
+                "trabajo": "Córdoba",
+                "testimonio": "Me considero una persona amante de la mala vida, amo tomar la vuelta sea cualquiera la ocación, pero lo que mas me gusta que lo hago por deporte ya es comer trabas de 2 pesos. EL alcohol arruino mi vida y solo tengo 27 años"
+            },
+            
+            {
+                "id": 2,
+                "nombre": "Porta",
+                "lugar": "La Falda city",
+                "imagen": "../imagenes/Ale",
+                "profesion": "Diablo",
+                "trabajo": "Cruz del Eje",
+                "testimonio": "Soy una atleta joven, me encanta tomar vodka, las mujeres y convertirme en Diablo por las noches. Me gusta salir al norte y comerme a norteñas que arrastran la r al hablar"
+            },
+            
+            
+            {
+                "id": 3,
+                "nombre": "Dani",
+                "lugar": "La Falda",
+                "imagen": "../imagenes/Dani",
+                "profesion": "Borrachin",
+                "trabajo": "Dique La Falda",
+                "testimonio": "Hola soy Daniel Jesus Quinteros, hace tiempo no me pego una derramada, pero cuando termine el bootcam me voy a reventar la mente tomando todo lo que me den. Me voy a convertir en el famoso Dani de la verdad"
+            },
+            
+            
+            {
+                "id": 4,
+                "nombre": "Guachin",
+                "lugar": "La Falda",
+                "imagen": "../imagenes/Guachin",
+                "profesion": "Comer todo lo que se mueva",
+                "trabajo": "todos lados",
+                "testimonio": "Me llamo Agustin, pero me dicen guachin porque soy guachin guachin. Me gusta cagarme a piñas con negros villeros en bares o restaurantes cada tanto, tomar merca del culo de mis amigos"
+            }
+        
+    ]
+}
+

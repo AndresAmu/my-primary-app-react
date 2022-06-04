833cdb27870d5cf93b5de426ec25a9d978e83369
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-06-04 13:41:00 -0300

    Vamos terminandos primer app

--- src/App.js
+++ src/App.js
@@ -13,28 +13,28 @@ function App() {
         imagen='Porta'
         profesion='Come traba de 2 pesos'
         trabajo='Córdoba'
-        testimonio='Me considero una persona amante de la mala vida, amo tomar la vuelta sea cualquiera la ocación, pero lo que mas me gusta que lo hago por deporte ya es comer trabas de 2 pesos. EL alcohol arruino mi vida y solo tengo 27 años'/>
+        testimonio='Me considero una persona amante de la mala vida, amo tomar la vuelta sea cualquiera la ocación, pero lo que mas me gusta que lo hago por deporte ya es comer trabas de 2 pesos. EL alcohol arruino mi vida y solo tengo 27 años' />
         <Testimonio
         nombre='Ale'
         lugar='La Falda city'
         imagen='Ale'
         profesion='Diablo'
         trabajo='Cruz del Eje'
-        testimonio='Soy una atleta joven, me encanta tomar vodka, las mujeres y convertirme en Diablo por las noches. Me gusta salir al norte y comerme a norteñas que arrastran la r al hablar'/>
+        testimonio='Soy una atleta joven, me encanta tomar vodka, las mujeres y convertirme en Diablo por las noches. Me gusta salir al norte y comerme a norteñas que arrastran la r al hablar' />
         <Testimonio
         nombre='Dani'
         lugar='La Falda'
         imagen='Dani'
         profesion='Borrachin'
         trabajo='Dique La Falda'
-        testimonio='Hola soy Daniel Jesus Quinteros, hace tiempo no me pego una derramada, pero cuando termine el bootcam me voy a reventar la mente tomando todo lo que me den. Me voy a convertir en el famoso Dani de la verdad '/>
+        testimonio='Hola soy Daniel Jesus Quinteros, hace tiempo no me pego una derramada, pero cuando termine el bootcam me voy a reventar la mente tomando todo lo que me den. Me voy a convertir en el famoso Dani de la verdad ' />
         <Testimonio
         nombre='Guachin'
         lugar='La Falda'
         imagen='Guachin'
         profesion='Comer todo lo que se mueva'
         trabajo='todos lados'
-        testimonio='Me llamo Agustin, pero me dicen guachin porque soy guachin guachin. Me gusta cagarme a piñas con negros villeros en bares o restaurantes cada tanto, tomar merca del culo de mis amigos'/>    
+        testimonio='Me llamo Agustin, pero me dicen guachin porque soy guachin guachin. Me gusta cagarme a piñas con negros villeros en bares o restaurantes cada tanto, tomar merca del culo de mis amigos' />    
       </div>
     </div>
   );
--- src/componentes/Testimonios.js
+++ src/componentes/Testimonios.js
@@ -1,20 +1,19 @@
 import React from "react";
 import '../stylesheets/Testimonios.css';
+
 function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img className="imagen-testimonio"
       src={require(`../imagenes/${props.imagen}.jpg`)}
-      alt='Porta Inmundo'/>
+      alt='Porta Inmundo' />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
           <strong>{props.nombre}</strong> en {props.lugar}</p>
-        <p className="cargo-testimonio">{props.profesion} en {props.trabajo}</p>
+        <p className="cargo-testimonio"><strong>{props.profesion}</strong> en {props.trabajo}</p>
         <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
-    
-    
   );
 }
 export default Testimonio;
